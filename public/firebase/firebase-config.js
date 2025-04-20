8fd62a48c8e137e9c678c53730b08bc26eec6505
mefeoku <promefe46@gmail.com>
2025-04-20 10:14:31 +0300

    firebase

--- public/firebase/firebase-config.js
+++ public/firebase/firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBEp6dsV_L5OpuJIXifBTEJ9moiRqGPfEU",
   authDomain: "artist-chat-2652b.firebaseapp.com",
   projectId: "artist-chat-2652b",
-  storageBucket: "artist-chat-2652b.appspot.com", // burada `.app` değil `.com` olmalı!
+  storageBucket: "artist-chat-2652b.appspot.app", 
   messagingSenderId: "773771165376",
   appId: "1:773771165376:web:d6c02ca28af6f5746b7725",
   measurementId: "G-96T97M1H40"
