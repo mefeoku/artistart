ff73b7c66e4537b65aef53700c4aa87634231c2e
mefeoku <promefe46@gmail.com>
2025-04-22 10:25:36 +0300

    Update firebase-config.js

--- public/firebase/firebase-config.js
+++ public/firebase/firebase-config.js
@@ -1,22 +1,27 @@
-// Firebase SDK modüllerini içe aktar
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { 
+  getFirestore,
+  doc,
+  setDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
-// Firebase ayarların
 const firebaseConfig = {
   apiKey: "AIzaSyBEp6dsV_L5OpuJIXifBTEJ9moiRqGPfEU",
   authDomain: "artist-chat-2652b.firebaseapp.com",
   projectId: "artist-chat-2652b",
-  storageBucket: "artist-chat-2652b.appspot.app", 
+  storageBucket: "artist-chat-2652b.appspot.com",
   messagingSenderId: "773771165376",
-  appId: "1:773771165376:web:d6c02ca28af6f5746b7725",
-  measurementId: "G-96T97M1H40"
+  appId: "1:773771165376:web:d6c02ca28af6f5746b7725"
 };
 
-// Firebase'i başlat
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Auth ve Firestore servislerini dışa aktar
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { auth, db, createUserWithEmailAndPassword, sendEmailVerification, doc, setDoc, serverTimestamp };
